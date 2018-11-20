b61cbe505f8069ddd052dae0638f3d8538f386e2
trykystm <trykystm@gmail.com>
2018-11-20 18:40:49 +0900

    Update zip_cloud.rb

--- zip_cloud.rb
+++ zip_cloud.rb
@@ -6,7 +6,8 @@ class ZipCloud
   URL = "http://zipcloud.ibsnet.co.jp/api/search/"
   
   def self.get(zipcode: , callback: nil)
-    query = {zipcode: zipcode, callback: callback}
+    query = {zipcode: zipcode}
+    query[:callback] = callback if callback
     hc = HTTPClient.new
     JSON.perse(hc.get(URL, query).body)
   end
