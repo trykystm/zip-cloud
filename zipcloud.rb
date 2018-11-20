a5d4de3144f855acda624b28f90f2e1dd3063e58
trykystm <trykystm@gmail.com>
2018-11-20 17:29:41 +0900

    Create zipcloud.rb

---
+++ zipcloud.rb
@@ -0,0 +1,10 @@
+require 'httpclient'
+require 'json'
+
+class Zipcloud
+URL = "http://zipcloud.ibsnet.co.jp/api/search"
+def self.get(zipcode: zipcode, callback: callback)
+hc = HTTPClient.new
+hc.get_content(URL, zipcode)
+end
+end
