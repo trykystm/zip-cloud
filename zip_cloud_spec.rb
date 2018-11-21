b2e982f490a4a0a22d97e3c10479c3161cb970ef
trykystm <trykystm@gmail.com>
2018-11-21 13:57:54 +0900

    Update zip_cloud_spec.rb

--- zip_cloud_spec.rb
+++ zip_cloud_spec.rb
@@ -4,11 +4,18 @@ require './zip_cloud'
 
 describe 'ZipCloud' do
   describe '#get' do
-    context "with argument 7830060" do
+    context 'with argument 7830060' do
       subject {ZipCloud.get zipcode: zipcode}
       let(:get){{:message=>nil, :results=>[{:address1=>"高知県", :address2=>"南国市", :address3=>"蛍が丘", :kana1=>"ｺｳﾁｹﾝ", :kana2=>"ﾅﾝｺｸｼ", :kana3=>"ﾎﾀﾙｶﾞｵｶ", :prefcode=>"39", :zipcode=>"7830060"}], :status=>200}}
       let(:zipcode){'7830060'}
       it {is_expected.to eq get}
     end
+    
+    context 'when argument is nothing' do
+    end
+    
+    context 'when argument is illegal' do
+    end
+    
   end
 end
