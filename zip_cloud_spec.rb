7cea6078577c2063915d3c525f3844d88f8595fd
trykystm <trykystm@gmail.com>
2018-11-22 16:48:28 +0900

    Update zip_cloud_spec.rb

--- zip_cloud_spec.rb
+++ zip_cloud_spec.rb
@@ -6,15 +6,17 @@ RSpec.describe ZipCloud do
     
     subject {ZipCloud.get zipcode: zipcode}
 
-    xcontext 'with argument 7830060' do
-      let(:get){{:address1=>"高知県", :address2=>"南国市", :address3=>"蛍が丘", :kana1=>"ｺｳﾁｹﾝ", :kana2=>"ﾅﾝｺｸｼ", :kana3=>"ﾎﾀﾙｶﾞｵｶ", :prefcode=>"39", :zipcode=>"7830060"}}
-      let(:zipcode){'7830060'}
-      it {is_expected.to eq get}
-    end
+    
     
     context 'address is not found' do
       let(:zipcode){'5368421'}
       it {is_expected.to be_nil}
+    end
+    
+    xcontext 'with argument 7830060' do
+      let(:get){{:address1=>"高知県", :address2=>"南国市", :address3=>"蛍が丘", :kana1=>"ｺｳﾁｹﾝ", :kana2=>"ﾅﾝｺｸｼ", :kana3=>"ﾎﾀﾙｶﾞｵｶ", :prefcode=>"39", :zipcode=>"7830060"}}
+      let(:zipcode){'7830060'}
+      it {is_expected.to eq get}
     end
     
     xcontext 'when argument is nothing' do
