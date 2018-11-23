98b99987d2ddcc68a5457bc4e2e1c6f7a5c4c3f8
trykystm <trykystm@gmail.com>
2018-11-23 19:46:01 +0900

    Update test_spec.rb

--- test_spec.rb
+++ test_spec.rb
@@ -1,17 +1,25 @@
 class Test
   def self.get arg
-    [{a: arg}].first
+    if arg
+      [{a: arg}].first
+    else
+      false
+    end
   end
 end
 
 RSpec.describe Test do
   describe '.get' do
     subject do
-      test = Test.get 'abc'
+      test = Test.get arg
     end
     
     context 'that return Array include Hash' do
       it {is_expected.to eq ({a: 'abc'})}
+    end
+    
+    context 'when arg is nil' do
+      let(:arg){nil}
     end
   end
 end
