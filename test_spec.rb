903d6706c6cd7067213c5015c231da14ac741a5a
trykystm <trykystm@gmail.com>
2018-11-23 19:38:45 +0900

    Update test_spec.rb

--- test_spec.rb
+++ test_spec.rb
@@ -1,17 +1,17 @@
 class Test
-  def self.get
-    [{a: 'a'}].first
+  def self.get arg
+    [{a: arg}].first
   end
 end
 
 RSpec.describe Test do
   describe '.get' do
     subject do
-      test = Test.get
+      test = Test.get 'abc'
     end
     
     context 'that return Array include Hash' do
-      it {is_expected.to eq ({a: 'a'})}
+      it {is_expected.to eq ({a: 'abc'})}
     end
   end
 end
